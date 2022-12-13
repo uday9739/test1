1838ca7191ec854a783ff20eea5b50dd8decb3bb
udaykumarbk <udaykumarbk@sptechteam.net>
2022-12-13 16:02:08 +0530

    deve

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello from sptech!\n'
+  const msg = 'Hello from sptechteam!\n'
   res.end(msg);
 });
 
